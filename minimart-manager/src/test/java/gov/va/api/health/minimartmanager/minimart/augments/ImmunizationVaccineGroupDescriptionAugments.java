02e1ad5ba0c9c360f5eda882afb98d519d23730a
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2022-01-31 11:25:04 -0500

    API-13101 add ImmunizationVaccineGroupDescriptionAugments (#283)

---
+++ minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/augments/ImmunizationVaccineGroupDescriptionAugments.java
@@ -0,0 +1,41 @@
+package gov.va.api.health.minimartmanager.minimart.augments;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.collect.Iterables;
+import gov.va.api.health.dataquery.service.controller.immunization.DatamartImmunization;
+import java.util.Objects;
+import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
+
+public class ImmunizationVaccineGroupDescriptionAugments {
+
+  public static void main(String[] args) {
+    Augmentation.forResources(DatamartImmunization.class)
+        .whenMatching(Objects::nonNull)
+        .transform(ImmunizationVaccineGroupDescriptionAugments::updateShortDescription)
+        .build()
+        .rewriteFiles();
+  }
+
+  static DatamartImmunization updateShortDescription(
+      Augmentation.Context<DatamartImmunization> ctx) {
+    DatamartImmunization dm = ctx.resource();
+
+    var coding = dm.vaccineCode().coding();
+    checkState(!coding.isEmpty());
+
+    if (coding.size() == 1) {
+      return dm;
+    }
+
+    var lastDisplay = Iterables.getLast(coding).display().get();
+
+    checkState(StringUtils.isNotBlank(lastDisplay));
+
+    lastDisplay = "VACCINE GROUP: " + lastDisplay;
+
+    Iterables.getLast(coding).display(Optional.of(lastDisplay));
+    return dm;
+  }
+}
